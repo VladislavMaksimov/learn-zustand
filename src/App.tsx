f46a2353ddae2beed97eb7fe28f6c9fa084abbf5
Vladislav Maksimov <workmaximov@yandex.ru>
2025-08-14 15:07:30 +0200

    add search params and AbortController

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
-import { Button, Card, Rate, Tag } from "antd";
+import { Button, Card, Input, Rate, Tag } from "antd";
 import "./App.css";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import { useCoffeeStore } from "./model/coffeeStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+
   const { coffeeList, getCoffeeList } = useCoffeeStore();
   useEffect(() => {
-    getCoffeeList();
-  }, [getCoffeeList]);
+    getCoffeeList({ text: searchText });
+  }, [getCoffeeList, searchText]);
   return (
-    <div className="cardsContainer">
-      {coffeeList?.map((coffee) => (
-        <Card
-          key={coffee.id}
-          cover={<img alt={coffee.name} src={coffee.image} />}
-          actions={[
-            <Button icon={<ShoppingCartOutlined />}>{coffee.price}</Button>,
-          ]}
-        >
-          <Card.Meta title={coffee.name} description={coffee.subTitle} />
-          <Tag color="purple" style={{ marginTop: 12 }}>
-            {coffee.type}
-          </Tag>
-          <Rate defaultValue={coffee.rating} disabled allowHalf />
-        </Card>
-      ))}
+    <div className="wrapper">
+      <Input
+        placeholder="поиск"
+        value={searchText}
+        onChange={(event) => {
+          setSearchText(event.target.value);
+        }}
+      />
+      <div className="cardsContainer">
+        {coffeeList?.map((coffee) => (
+          <Card
+            key={coffee.id}
+            cover={<img alt={coffee.name} src={coffee.image} />}
+            actions={[
+              <Button icon={<ShoppingCartOutlined />}>{coffee.price}</Button>,
+            ]}
+          >
+            <Card.Meta title={coffee.name} description={coffee.subTitle} />
+            <Tag color="purple" style={{ marginTop: 12 }}>
+              {coffee.type}
+            </Tag>
+            <Rate defaultValue={coffee.rating} disabled allowHalf />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- src/model/coffeeStore.ts
+++ src/model/coffeeStore.ts
@@ -1,29 +1,51 @@
 import { create, type StateCreator } from "zustand";
-import type { CoffeeType } from "../types/coffeeTypes";
+import type {
+  CoffeeType,
+  GetCoffeeListRequestParams,
+} from "../types/coffeeTypes";
 import { devtools } from "zustand/middleware";
 
 const BASE_URL = "https://purpleschool.ru/coffee-api";
 
 type CoffeeState = {
   coffeeList?: CoffeeType[];
+  controller?: AbortController;
 };
 
 type CoffeeActions = {
-  getCoffeeList: VoidFunction;
+  getCoffeeList: (params?: GetCoffeeListRequestParams) => Promise<void>;
 };
 
 type CoffeeStore = CoffeeState & CoffeeActions;
 
 const coffeeSlice: StateCreator<CoffeeStore, [["zustand/devtools", never]]> = (
-  set
+  set,
+  get
 ) => ({
   coffeeList: undefined,
-  getCoffeeList: async () => {
+  controller: undefined,
+  getCoffeeList: async (params) => {
     try {
-      const response = await fetch(BASE_URL);
+      const { controller } = get();
+      if (controller) {
+        controller.abort();
+      }
+      const newController = new AbortController();
+      const { signal } = newController;
+      set({ controller: newController });
+
+      const searchParams = new URLSearchParams();
+      if (params && params.text) {
+        searchParams.append("text", params.text);
+      }
+
+      const response = await fetch(`${BASE_URL}?${searchParams.toString()}`, {
+        signal,
+      });
       if (!response.ok) {
         throw new Error("Failed to fetch coffee list");
       }
+
       const data = await response.json();
       set(
         {
